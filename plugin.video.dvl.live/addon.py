556be481a6c6b8d2c70579fab21aded9262b53a8
Friedrich Fell <friedrich.fell87@gmail.com>
2013-10-29 00:04:42 +0100

    Unterseiten hinzugefügt; Alle Seiten auslesen; Kommentare eingefügt;

--- plugin.video.dvl.live/addon.py
+++ plugin.video.dvl.live/addon.py
@@ -7,9 +7,10 @@ Created on 28.10.2013
 import urllib,urllib2,re,xbmcplugin,xbmcgui
 
 def CATEGORIES():
-        addDir('Maenner','http://www.dvllive.tv/videos',2,'')
-        addDir( 'Frauen','http://www.dvllive.tv/videos',2,'')
-        addDir( '2.Liga','http://www.dvllive.tv/videos',2,'')
+        addDir('Aktuell','http://www.dvllive.tv/videos',2,'')
+        addDir('Männer','http://www.dvllive.tv/videos?tags%5Bliga%5D%5B%5D=Männer',2,'')
+        addDir( 'Frauen','http://www.dvllive.tv/videos?tags%5Bliga%5D%5B%5D=Frauen',2,'')
+        addDir( '2.Liga','http://www.dvllive.tv/videos?tags%5Bliga%5D%5B%5D=2.+Liga',2,'')
                        
 def INDEX(url):
         req = urllib2.Request(url)
@@ -22,23 +23,38 @@ def INDEX(url):
                 addDir(name,url,1,thumbnail)
                 
 def VIDEOLINKS(url,name):
-        req = urllib2.Request(url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()
-        match=re.compile(r'<a href="(.+?)" title="(.+?)">(?:\n|\r\n?)                    <span class="thumb mini">(?:\n|\r\n?)                      <img alt=".+?" src="(.+?)" \/>').findall(link)
-
-        for url,name,thumb in match:
-                url = "http://www.dvllive.tv"+url
-                req1 = urllib2.Request(url)
-                req1.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-                response1 = urllib2.urlopen(req1)
-                link1=response1.read()
-                response1.close()
-                match1=re.compile(r'data-version=".+?" href="(.+?)"').findall(link1)
-                
-                addLink(name,match1[0],thumb)
+        
+        newPage = 1;
+        
+        #Schleife um alle Seiten abzufragen
+        while newPage == 1:
+            #Links der Videoseiten aus der Übersicht auslesen
+            req = urllib2.Request(url)
+            req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+            response = urllib2.urlopen(req)
+            link=response.read()
+            response.close()
+            match=re.compile(r'<a href="(.+?)" title="(.+?)">(?:\n|\r\n?)                    <span class="thumb mini">(?:\n|\r\n?)                      <img alt=".+?" src="(.+?)" \/>').findall(link)
+            
+            #Link zu Video aus Videoseite auslesen
+            for url,name,thumb in match:
+                    url = "http://www.dvllive.tv"+url
+                    req1 = urllib2.Request(url)
+                    req1.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+                    response1 = urllib2.urlopen(req1)
+                    link1=response1.read()
+                    response1.close()
+                    match1=re.compile(r'data-version=".+?" href="(.+?)"').findall(link1)
+                    #Video Link hinzufügen
+                    addLink(name,match1[0],thumb)
+            
+            #Überprüfen ob weitere Seiten vorhanden sind
+            nextPage=re.compile(r'<a class="next_page" data-remote="true" rel="next" href="(.+?)">').findall(link)
+            if len(nextPage) == 0:
+                newPage = 0
+            else:
+                #nächste Seite in url Variable schreiben
+                url = "http://www.dvllive.tv"+nextPage[0]
         
 
                 

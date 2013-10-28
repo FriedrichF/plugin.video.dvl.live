154bf9a066eb931365c43cc250019d047dcb3e68
Friedrich Fell <friedrich.fell87@gmail.com>
2013-10-28 22:32:09 +0100

    First commit

---
+++ plugin.video.dvl.live/addon.py
@@ -0,0 +1,115 @@
+import urllib,urllib2,re,xbmcplugin,xbmcgui
+
+#TV DASH - by You 2008.
+
+def CATEGORIES():
+        addDir('Maenner','http://www.dvllive.tv/videos',2,'')
+        addDir( 'Frauen','http://www.dvllive.tv/videos',2,'')
+        addDir( '2.Liga','http://www.dvllive.tv/videos',2,'')
+                       
+def INDEX(url):
+        req = urllib2.Request(url)
+        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+        response = urllib2.urlopen(req)
+        link=response.read()
+        response.close()
+        match=re.compile('').findall(link)
+        for thumbnail,url,name in match:
+                addDir(name,url,1,thumbnail)
+                
+def VIDEOLINKS(url,name):
+        req = urllib2.Request(url)
+        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+        response = urllib2.urlopen(req)
+        link=response.read()
+        response.close()
+        match=re.compile(r'<a href="(.+?)" title="(.+?)">(?:\n|\r\n?)                    <span class="thumb mini">(?:\n|\r\n?)                      <img alt=".+?" src="(.+?)" \/>').findall(link)
+
+        for url,name,thumb in match:
+                url = "http://www.dvllive.tv"+url
+                req1 = urllib2.Request(url)
+                req1.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+                response1 = urllib2.urlopen(req1)
+                link1=response1.read()
+                response1.close()
+                match1=re.compile(r'data-version=".+?" href="(.+?)"').findall(link1)
+                
+                addLink(name,match1[0],thumb)
+        
+
+                
+def get_params():
+        param=[]
+        paramstring=sys.argv[2]
+        if len(paramstring)>=2:
+                params=sys.argv[2]
+                cleanedparams=params.replace('?','')
+                if (params[len(params)-1]=='/'):
+                        params=params[0:len(params)-2]
+                pairsofparams=cleanedparams.split('&')
+                param={}
+                for i in range(len(pairsofparams)):
+                        splitparams={}
+                        splitparams=pairsofparams[i].split('=')
+                        if (len(splitparams))==2:
+                                param[splitparams[0]]=splitparams[1]
+                                
+        return param
+
+
+
+
+def addLink(name,url,iconimage):
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name } )
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
+        return ok
+
+
+def addDir(name,url,mode,iconimage):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name } )
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        return ok
+        
+              
+params=get_params()
+url=None
+name=None
+mode=None
+
+try:
+        url=urllib.unquote_plus(params["url"])
+except:
+        pass
+try:
+        name=urllib.unquote_plus(params["name"])
+except:
+        pass
+try:
+        mode=int(params["mode"])
+except:
+        pass
+
+print "Mode: "+str(mode)
+print "URL: "+str(url)
+print "Name: "+str(name)
+
+if mode==None or url==None or len(url)<1:
+        print ""
+        CATEGORIES()
+       
+elif mode==1:
+        print ""+url
+        INDEX(url)
+        
+elif mode==2:
+        print ""+url
+        VIDEOLINKS(url,name)
+
+
+
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
